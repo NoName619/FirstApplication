39ae0e3a9e2942d78aacaffc2e5062ce32c47025
Name <email@example.ru>
2024-01-07 04:16:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleHelper/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace VoiceApplication.ConsoleHelper
+{
+    public class Helper
+    {
+        public static void Help()
+        {
+            Console.WriteLine("Помогаю");
+        }
+    }
+}
+
+
---
+++ FirstApplication.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName;
+myName = "Jane";
+
+Console.WriteLine(myName);
+
+Console.ReadKey();
+
+
+
